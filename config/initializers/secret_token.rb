015df101d5b3ac2ca913f4317178fafe8219b301
Zamith <zamith.28@gmail.com>
2012-08-24 16:25:11 +0100

    From scratch

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MySite::Application.config.secret_token = '4f6145827376dd8c32fe15358d753d8c666bb8518372da529799b17cf4db658e214bc77035b4c85efcc2dc4d953c7b4bd997400edf587960e6e6bd2e56980f56'
---
+++ zamith.pt/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZamithPt::Application.config.secret_token = '4524400cade5c766074a52f5401e2247b8e6b0e5caccb964c26cc1f95ccd17f223e6846de63f37aa129fe1454c6fe7357aff95fdf44e48e12e08663a4705e2ce'
